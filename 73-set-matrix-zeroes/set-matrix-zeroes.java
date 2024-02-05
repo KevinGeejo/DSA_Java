4e52620dc336a83239cc709767ec362631aea476
Kevin <74069952+KevinGeejo@users.noreply.github.com>
2024-02-05 18:40:09 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (16.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,29 @@
+// Approach Two 
+
 class Solution {
     public void setZeroes(int[][] matrix) {
 
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int[] row = new int[n];
-        int[] col = new int[m];
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
 
-        // Traverse matrix and set new row and col arr to 1 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m;j++){
+
+        for(int i = 0; i<n ; i++){
+            for(int j =0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
 
 
-        // Set Values to 0 in orginal matrix by comparing with row and col arr.
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(row[i] == 1 || col[j] == 1){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(row[i] == true || col[j] == true){
                     matrix[i][j] = 0;
-                   
                 }
             }
         }
