469b244d053931155601345b33f259181b014208
Kevin <74069952+KevinGeejo@users.noreply.github.com>
2025-03-31 19:12:40 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (79.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,36 +3,22 @@ class Solution {
         if(nums.length<2){
             return nums[0];
         }
+    int[] range1 = Arrays.copyOfRange(nums, 1, nums.length);
+    int[] range2 = Arrays.copyOfRange(nums, 0, nums.length-1);
 
-        int[] lastskip = new int[nums.length-1];
-        int[] firstskip = new int[nums.length-1];
-
-        for(int i =0; i<nums.length-1;i++){
-            lastskip[i] = nums[i];
-            firstskip[i] = nums[i+1];
-        }
-
-        int sumlastskip = robHelper(lastskip);
-        int sumfirstskip = robHelper(firstskip);
-
-        return Math.max(sumlastskip,sumfirstskip);
+        int result1 = robHelper(range1);
+         int result2 = robHelper(range2);
+      int ans = Math.max(result1,result2);
+      return ans;
     }
-
-    private int robHelper(int[] nums){
-        if(nums.length <2){
-            return nums[0];
+    public int robHelper(int[] nums){
+         int x = 0, y = 0;
+        for(int n : nums){
+            int temp = Math.max(n+x, y);
+            x = y;
+            y = temp;
+           
         }
-
-        int[] dp = new int[nums.length];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for(int i =2; i<nums.length;i++){
-            dp[i] = Math.max(dp[i-2] + nums[i] , dp[i-1]);
-        }
-
-        return dp[nums.length-1];
-
+        return y;
     }
 }
